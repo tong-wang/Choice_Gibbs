1d9796700fc1ff3e1d36d9faddea4c45fd7b48d6
Tong Wang <tong.wang@nus.edu.sg>
2014-05-16 15:56:30 +0800

    new Init_Data with traffic info built-in

--- src/binary.L1/MNL_InitData.binary.L1.R
+++ src/binary.L1/MNL_InitData.binary.L1.R
@@ -5,6 +5,10 @@
 # --- with inventory and stock-out info
 ####
 
+#clear environment
+rm(list=ls(all.names=TRUE))
+
+
 Sys.setenv(LANG = "en")
 setwd("~/Dropbox/RCode/Choice_Gibbs.git/src/binary.L1")
 
@@ -27,7 +31,7 @@ X_Mat <- rmvnorm(K, mean=X_Mean)
 
 ## true values of the parameters to be estimated
 # beta is the MNL coefficient
-beta <- c(0.1); # L-dimensional
+beta <- c(0.3); # L-dimensional
 # lambda is the poisson demand rate in each perid
 lambda <- 50
 
@@ -50,12 +54,29 @@ for (k in 1:K) {
 rowMeans(choice.mat)
 
 
+### organize data for model use
 Inventory <- round(runif(K, min=20, max=30))
 Demand <- choice.mat[1:M-1,]
 Sales <- pmin(Demand, Inventory)
 Stockout <- (Demand >= Inventory)
 LostSales <- pmax(Demand-Inventory, 0)
 NoPurchase <- choice.mat[M,]
+
+
+### simulate traffic data with different accuracy
+epsilon1.mean <- 3
+epsilon1.xl <- rnorm(K, mean=epsilon1.mean, sd=1)
+epsilon1.l <- rnorm(K, mean=epsilon1.mean, sd=0.5)
+epsilon1.m <- rnorm(K, mean=epsilon1.mean, sd=0.1)
+epsilon1.h <- rnorm(K, mean=epsilon1.mean, sd=0.05)
+epsilon1.xh <- rnorm(K, mean=epsilon1.mean, sd=0.01)
+
+Traffic.xl <- exp(epsilon1.xl) * colSums(choice.mat) 
+Traffic.l <- exp(epsilon1.l) * colSums(choice.mat) 
+Traffic.m <- exp(epsilon1.m) * colSums(choice.mat) 
+Traffic.h <- exp(epsilon1.h) * colSums(choice.mat) 
+Traffic.xh <- exp(epsilon1.xh) * colSums(choice.mat) 
+
 
 
 rm(k)
--- src/binary/MNL_InitData.binary.R
+++ src/binary/MNL_InitData.binary.R
@@ -4,6 +4,10 @@
 # --- with inventory and stock-out info
 ####
 
+#clear environment
+rm(list=ls(all.names=TRUE))
+
+
 Sys.setenv(LANG = "en")
 setwd("~/Dropbox/RCode/Choice_Gibbs.git/src/binary")
 
@@ -26,7 +30,7 @@ X_Mat <- rmvnorm(K, mean=X_Mean)
 
 ## true values of the parameters to be estimated
 # beta is the MNL coefficient
-beta <- c(0.1, 0.05); # L-dimensional
+beta <- c(0.3, 0.1); # L-dimensional
 # lambda is the poisson demand rate in each perid
 lambda <- 50
 
@@ -55,6 +59,22 @@ Sales <- pmin(Demand, Inventory)
 Stockout <- (Demand >= Inventory)
 LostSales <- pmax(Demand-Inventory, 0)
 NoPurchase <- choice.mat[M,]
+
+
+### simulate traffic data with different accuracy
+epsilon1.mean <- 3
+epsilon1.xl <- rnorm(K, mean=epsilon1.mean, sd=1)
+epsilon1.l <- rnorm(K, mean=epsilon1.mean, sd=0.5)
+epsilon1.m <- rnorm(K, mean=epsilon1.mean, sd=0.1)
+epsilon1.h <- rnorm(K, mean=epsilon1.mean, sd=0.05)
+epsilon1.xh <- rnorm(K, mean=epsilon1.mean, sd=0.01)
+
+Traffic.xl <- exp(epsilon1.xl) * colSums(choice.mat) 
+Traffic.l <- exp(epsilon1.l) * colSums(choice.mat) 
+Traffic.m <- exp(epsilon1.m) * colSums(choice.mat) 
+Traffic.h <- exp(epsilon1.h) * colSums(choice.mat) 
+Traffic.xh <- exp(epsilon1.xh) * colSums(choice.mat) 
+
 
 
 rm(k)
